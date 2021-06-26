8a0647ade2f68b6a67b0634eb1771ae8e382fa86
eclecticcoding <chuck@eclecticsaddlebag.com>
2021-06-26 11:39:42 -0400

    Set up bootstrap and webpacker

--- Gemfile
+++ Gemfile
@@ -12,7 +12,6 @@ gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.7"
 gem "bootsnap", ">= 1.4.4", require: false
 # ===== App specific gems ========================
-gem "pg"
 gem "faker"
 
 group :development, :test do
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,4 +11,11 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-        import "../stylesheets/application.scss"
+
+var jQuery = require("jquery")
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+require('bootstrap');
+require('admin-lte')
+
+import "../stylesheets/application.scss"
